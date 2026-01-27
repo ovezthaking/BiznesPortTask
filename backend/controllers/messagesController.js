2f32adfb04f5194a7d1846a518e3a084e149b51e
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-01-27 22:25:30 +0100

    feat: add implementation. error handling and validation for createMessage function

--- backend/controllers/messagesController.js
+++ backend/controllers/messagesController.js
@@ -1,3 +1,4 @@
+import { validationResult } from "express-validator"
 import Message from "../models/Message.js"
 import express from 'express'
 
@@ -39,5 +40,30 @@ export const getMessageById = async (req, res, next) => {
 }
 
 export const createMessage = async (req, res, next) => {
-    
+    try {
+        const errors = validationResult(req)
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                success: false,
+                errors: errors.array()
+            })
+        }
+
+        const { content } = req.body
+
+        const message = await Message.create({
+            content
+        })
+
+        res.status(201).json({
+            success: true,
+            data: message,
+            message: 'Wiadomość pomyślnie utworzona'
+        })
+    } catch (err) {
+        next(err)
+    }
 }
+
+
