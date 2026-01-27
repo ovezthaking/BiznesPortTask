d759e827a6e408421e00cb03ffc68fa1720c8467
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-01-27 22:49:36 +0100

    feat: add validation middleware to createMessage and updateMessage routes

--- backend/routes/messages.js
+++ backend/routes/messages.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import { createMessage, getAllMessages, getMessageById, updateMessage } from '../controllers/messagesController.js'
+import { validateMessage } from '../middleware/validateMessage.js'
 
 export const messagesRouter = express.Router()
 
 messagesRouter.get('/', getAllMessages)
 messagesRouter.get('/:messageId', getMessageById)
 
-messagesRouter.post('/', createMessage)
+messagesRouter.post('/', validateMessage, createMessage)
 
-messagesRouter.put('/:messageId', updateMessage)
+messagesRouter.put('/:messageId', validateMessage, updateMessage)
