20e97c9a10aa5e95098f90716fdf4cb03f6dd461
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-01-27 02:53:06 +0100

    feat: implement getAllMessages function to retrieve messages in descending order

---
+++ backend/controllers/messagesController.js
@@ -0,0 +1,16 @@
+import Message from "../models/Message"
+
+export const getAllMessages = async (req, res, next) => {
+    try {
+        const messages = await Message.findAll({
+            order: [['createdAt', 'DESC']]
+        })
+
+        res.json({
+            success: true,
+            data: messages
+        })
+    } catch (e) {
+        next(error)
+    }
+}
