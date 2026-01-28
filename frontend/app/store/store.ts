5180516a110c00a27ef06e084c60becc9622ac7d
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-01-28 21:37:09 +0100

    feat: add Redux store configuration with messages API integration

---
+++ frontend/app/store/store.ts
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { messagesApi } from "./services/messagesApi.js";
+
+export const store = configureStore({
+    reducer: {
+        [messagesApi.reducerPath]: messagesApi.reducer
+    },
+    middleware: (getDefaultMiddleware) => 
+        getDefaultMiddleware().concat(messagesApi.middleware)
+})
+
+export type AppStore = typeof store
+export type RootState = ReturnType<AppStore['getState']>
+export type AppDispatch = AppStore['dispatch']
