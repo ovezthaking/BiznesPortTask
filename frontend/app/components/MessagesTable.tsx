b802e7648de1620287cefdccdace63e679849d4c
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-01-28 22:38:38 +0100

    feat: add CORS support and enhance MessagesTable component with loading and error states

--- frontend/app/components/MessagesTable.tsx
+++ frontend/app/components/MessagesTable.tsx
@@ -1,4 +1,38 @@
+'use client'
+
+import { useState } from "react"
+import { useGetMessagesQuery } from "../store/services/messagesApi"
+import { Message } from "../store/types"
+
 export default function MessagesTable() {
+    const { data: messages, isLoading, error } = useGetMessagesQuery()
+    const [editMessage, setEditMessage] = useState<Message | null>(null)
+    const [deleteMessage, setDeleteMessage] = useState<Message | null>(null)
+
+    if (isLoading){
+        return(
+            <div>
+                <span>Ładowanie wiadomości...</span>
+            </div>
+        )
+    }
+
+    if (error) {
+        return (
+            <div>
+                <p>Błąd ładowania wiadomości</p>
+            </div>
+        )
+    }
+
+    if (!messages || messages.length === 0) {
+        return (
+            <div>
+                <p>Brak wiadomości. Dodaj pierwszą wiadomość!</p>
+            </div>
+        )
+    }
+
     return (
         <p>Message Table</p>
     )
