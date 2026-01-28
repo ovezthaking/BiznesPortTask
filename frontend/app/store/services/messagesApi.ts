1d145356dd40bfec5a8aee927d7bb404a57b972e
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-01-28 20:56:36 +0100

    refactor: remove unused imports in messagesApi service

--- frontend/app/store/services/messagesApi.ts
+++ frontend/app/store/services/messagesApi.ts
@@ -1,5 +1,5 @@
 import type { Message, MessageResponse } from "../types.js";
-import { CreateApi, createApi, EndpointDefinitions, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const messagesApi = createApi({
     reducerPath: 'messagesApi',
