31106326f7f058539d3db1f2415aee2d7e8008d2
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-01-29 18:45:46 +0100

    stylization of main page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,10 +5,10 @@ import MessagesTable from "./components/MessagesTable";
 export default function Home() {
   return (
     <>
-      <header className="bg-primary p-8 pl-12 text-primary-foreground">
+      <header className="bg-primary p-6 pl-12 text-primary-foreground w-full">
         <div className="mx-auto">
           <div className="">
-            <h1 className="text-4xl font-bold">
+            <h1 className="text-3xl font-bold">
               Wiadomości
             </h1>
             <p className="text-gray-300 mt-2">
@@ -19,13 +19,17 @@ export default function Home() {
       </header>
 
       <main className="min-h-screen bg-primary/20 p-8">
-        <div className="max-w-6xl mx-auto space-y-8">
+        <div className="max-w-6xl mx-auto space-y-8 mt-10 mb-10">
           <MessageForm />
 
   
           <MessagesTable />
         </div>
       </main>
+
+      <footer className="bg-primary p-6 pl-12 text-primary-foreground w-full text-center">
+        <p className="text-sm">2026 © Oliwer Urbaniak</p>
+      </footer>
     </>
   )
 }
