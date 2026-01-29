71b1744a860ef0d46cfa85a0e4592a47576e65a0
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-01-29 16:57:37 +0100

    feat: update Home component styling for improved visual consistency

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,20 +5,20 @@ import MessagesTable from "./components/MessagesTable";
 export default function Home() {
   return (
     <>
-      <header className="bg-gray-50 p-8">
-        <div className="max-w-6xl mx-auto">
-          <div className="text-center">
-            <h1 className="text-4xl font-bold text-gray-900">
+      <header className="bg-primary p-8 pl-12 text-primary-foreground">
+        <div className="mx-auto">
+          <div className="">
+            <h1 className="text-4xl font-bold">
               Wiadomości
             </h1>
-            <p className="text-gray-600 mt-2">
+            <p className="text-gray-300 mt-2">
               Zarządzaj swoimi wiadomościami
             </p>
           </div>
         </div>
       </header>
 
-      <main className="min-h-screen bg-gray-50 p-8">
+      <main className="min-h-screen bg-primary/20 p-8">
         <div className="max-w-6xl mx-auto space-y-8">
           <MessageForm />
 
